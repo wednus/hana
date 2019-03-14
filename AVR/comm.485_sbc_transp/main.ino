edd980c524101d24751cfb98838bbe98125c5f09
Sundew Shin <sundew@wednus.com>
2019-03-14 08:04:43 +0000

    misc update

--- AVR/comm.485_sbc_transp/main.ino
+++ AVR/comm.485_sbc_transp/main.ino
@@ -1,13 +1,25 @@
-#include <AltSoftSerial.h>
-AltSoftSerial  xbSerial;        // RX:8, TX:9, RE:10 (orig. PWM)
+#include <SoftwareSerial.h>
+
+SoftwareSerial rsSerial(3, 4);  // RX:3, TX:4, RE:7
+int rePin = 7;
 
 void setup(){
   Serial.begin(9600);
-  xbSerial.begin(9600);
+  rsSerial.begin(9600);
+  pinMode(rePin, OUTPUT); // RE for RS485
+  pinMode(2, OUTPUT);     // RE for SBC   
 }
 
 
 void loop(){
-  if (Serial.available()) xbSerial.write(Serial.read());
-  if (xbSerial.available()) Serial.write(xbSerial.read());
+  if (Serial.available()){
+    digitalWrite(rePin, HIGH);
+    rsSerial.write(Serial.read());    // SBC > 485
+    digitalWrite(rePin, LOW);
+    rsSerial.flush();
+  }
+  if (rsSerial.available()){
+    Serial.write(rsSerial.read());  // 485 > SBC
+    Serial.flush();
+  }
 }
