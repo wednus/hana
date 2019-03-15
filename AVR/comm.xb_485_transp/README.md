1f995b4d78622ed248368f7b77a5a3a37151d739
Sundew Shin <sundew@wednus.com>
2019-03-15 10:41:08 +0000

    changed string to char array for better performance

--- AVR/comm.xb_485_transp/README.md
+++ AVR/comm.xb_485_transp/README.md
@@ -1,6 +1,6 @@
 XBee-RS485 Transparent
 =
-***Maturity: Alpha***
+***Maturity: Beta***
 
 The purpose of this program is turnerling through XBee and RS485, so whatever data come in one end forwarded to the other end transparently.
-Once it is fully implemented it can be used for Hana in stand-alone mode connecting a remote RS485 supporting equipment over wireless.
+Tt can be used for Hana in stand-alone mode connecting a remote RS485 supporting equipment over wireless.
--- AVR/comm.xb_485_transp/main.ino
+++ AVR/comm.xb_485_transp/main.ino
@@ -1,27 +1,43 @@
-#include <SoftwareSerial.h>
 #include <AltSoftSerial.h>
+#include <SoftwareSerial.h>
 
 AltSoftSerial  xbSerial;        // RX:8, TX:9, RE:10 (orig. PWM)
 SoftwareSerial spSerial(3, 4);  // RX:3, TX:4, RE:7
 int rePin = 7;
+const int baud = 9600;
+const int maxBufSize = 100;
+char buf[maxBufSize] = "";
+int index = 0;
 
 void setup(){
-  xbSerial.begin(9600);   // Comm for XB
-  spSerial.begin(9600);   // Comm for RS485
+  Serial.begin(baud);
+  xbSerial.begin(baud);   // Comm for XB
+  spSerial.begin(baud);   // Comm for RS485
   pinMode(rePin, OUTPUT); // RE for RS485
-  pinMode(2, OUTPUT);     // RE for SBC
 }
 
 
 void loop(){
   if (spSerial.available()){
-    digitalWrite(rePin, HIGH);
-    spSerial.write(xbSerial.read());  // XB > 485
-    spSerial.flush();                 // wait for the trasnmission of outgoing data
-    digitalWrite(rePin, LOW);
-  }
-  if (spSerial.available()){
     xbSerial.write(spSerial.read());  // 485 > XB
-    xbSerial.flush();
+  }
+
+  if(xbSerial.available()){
+    if(index < maxBufSize){  // prevent out-of-boundary error
+      buf[index++] = xbSerial.read();
+    }else{
+      index = 0;
+    }
+  }else if(index != 0){
+    //Serial.write((const uint8_t*)buf, index);
+    //Serial.flush();
+    //delay(100);
+    delay(3000);  // adjust delay per the response timing
+    
+    PORTD |= B10000000;
+    spSerial.write((const uint8_t*)buf, index);
+    index = 0;
+    spSerial.flush();
+    PORTD &= B01111111;
   }
 }
