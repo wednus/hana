3f60ccc4bec3943b54fa918e6e45b3e65c95c82e
Sundew Shin <sundew@wednus.com>
2019-04-01 08:39:35 +0100

    changed hex->bin

--- AVR/prog.firmware/download.sh
+++ AVR/prog.firmware/download.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# This script download the AVR's flash contents into firmware.hex.
+# This script download the AVR's flash contents into firmware.bin.
 
 # backup existing
-if [ -f firmware.hex ]; then
-  mv firmware.hex firmware_backup_$(date +'%m%d%y_%H%M%S').hex
+if [ -f firmware.bin ]; then
+  mv firmware.bin firmware_backup_$(date +'%m%d%y_%H%M%S').bin
 fi
 
-avrdude -p m328p -P /dev/serial0 -c arduino -b 57600 -U flash:r:firmware.hex:r
+avrdude -p m328p -P /dev/serial0 -c arduino -b 57600 -U flash:r:firmware.bin:r
